496917724ad8886e87ddd8bb51f09cdea635dc81
Bklzn <adrian_bon@wp.pl>
2025-10-23 00:31:20 +0200

    add some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component, effect, inject } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  effect,
+  ElementRef,
+  inject,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { Title } from '@angular/platform-browser';
 import { TodoService } from './services/todo.service';
 import { TodoAddFormComponent } from './components/todo-add-form/todo-add-form.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [TodoItemComponent, TodoAddFormComponent],
+  imports: [TodoItemComponent, TodoAddFormComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,20 +1,25 @@
-import { NgClass } from '@angular/common';
-import { Component, input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  input,
+  Output,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { Todo } from '../../model/todo.type';
+import { Collapse } from 'bootstrap';
 
 @Component({
-  selector: 'app-todo-item',
-  imports: [NgClass],
+  selector: '[app-todo-item]',
+  imports: [],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
 })
 export class TodoItemComponent {
   task = input.required<Todo>();
-  descVisible: boolean = false;
-
-  toggleDescription() {
-    this.descVisible = !this.descVisible;
-  }
+  index = input.required<number>();
 
   toggleCompleted(task: any) {
     task.status = task.status === 'Completed' ? 'Planned' : 'Completed';
