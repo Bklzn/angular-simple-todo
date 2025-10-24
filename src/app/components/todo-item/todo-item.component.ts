ea7efe0bad845875eb7ef8c45aad3aff9593b34c
Bklzn <adrian_bon@wp.pl>
2025-10-24 12:22:53 +0200

    handle more statuses, fix styles

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { Todo } from '../../model/todo.type';
 import { TodoEditItemComponent } from '../todo-edit-item/todo-edit-item.component';
 import { TodoDeleteItemComponent } from '../todo-delete-item/todo-delete-item.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: '[app-todo-item]',
-  imports: [TodoEditItemComponent, TodoDeleteItemComponent],
+  imports: [TodoEditItemComponent, TodoDeleteItemComponent, NgClass],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
 })
@@ -13,7 +14,13 @@ export class TodoItemComponent {
   task = input.required<Todo>();
   index = input.required<number>();
 
-  toggleCompleted(task: any) {
-    task.status = task.status === 'Completed' ? 'Planned' : 'Completed';
+  toggleStatus(task: any) {
+    if (task.status === 'Completed') {
+      task.status = 'Planned';
+    } else if (task.status === 'Planned') {
+      task.status = 'Pending';
+    } else {
+      task.status = 'Completed';
+    }
   }
 }
